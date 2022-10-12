73d7e59ddfa1a8ef8627df396352a5181e205edb
funasedaisuke <dai.boy.human@gmail..com>
2022-10-12 10:18:36 +0900

    improve API client and preserve data in db

--- db/db.go
+++ db/db.go
@@ -1,8 +1,8 @@
-package main
+package db
 
 import (
 	// "database/sql"
-	"encoding/json"
+
 	"fmt"
 	"time"
 
@@ -14,6 +14,7 @@ type Comment struct {
 	LinkCnt      int       `json:"linkCnt"`
 	Comment      string    `json:"comment"`
 	Updated_time time.Time `json:"time"`
+	VideoID      string    `json:"videoid"`
 }
 
 func NewMysqlConnect(dsn string) (*sqlx.DB, error) {
@@ -29,7 +30,7 @@ func NewMysqlConnect(dsn string) (*sqlx.DB, error) {
 	return DB, nil
 
 }
-func dbxSelect(dbx *sqlx.DB, query string) {
+func DbxSelect(dbx *sqlx.DB, query string) {
 	result := []Comment{}
 	//"select * from tests"
 	dbx.Select(&result, query)
@@ -38,22 +39,22 @@ func dbxSelect(dbx *sqlx.DB, query string) {
 	}
 }
 
-func dbxInsert(dbx *sqlx.DB, jsonbody string) {
+func DbxInsert(dbx *sqlx.DB, comments []Comment) {
 
 	//jsonbody := `{ "linkCnt":0,"comment":"good","time": "2022-06-25T00:22:50Z"}`
 	//jsonarraybody := `[{ "linkCnt":0,"comment":"good","time": "2022-06-25T00:22:50Z"},{ "linkCnt":2,"comment":"nice","time": "2022-06-25T00:22:50Z"}]`
 	tx := dbx.MustBegin()
 	var tablename = "jarujaruch"
 
-	var comments []Comment
-	err := json.Unmarshal([]byte(jsonbody), &comments)
-	if err != nil {
-		fmt.Print(err)
-	}
+	// var comments []Comment
+	// err := json.Unmarshal([]byte(jsonbody), &comments)
+	// if err != nil {
+	// 	fmt.Print(err)
+	// }
 	for _, item := range comments {
 		// query := fmt.Sprintf("insert into %s(comment,likecnt,updated_tiime) values(%s,%d,%s)", tablename, item.Comment, item.LinkCnt, item.Updated_time)
 
-		query := fmt.Sprintf("insert into %s (comment, likecnt,updated_time) values (\"%s\",%d,\"%s\")", tablename, item.Comment, item.LinkCnt, item.Updated_time.Format("2006-01-02 15:04:05"))
+		query := fmt.Sprintf("insert into %s (comment, likecnt,updated_time,videoid) values (\"%s\",%d,\"%s\",\"%s\")", tablename, item.Comment, item.LinkCnt, item.Updated_time.Format("2006-01-02 15:04:05"), item.VideoID)
 
 		fmt.Print(query)
 		tx.MustExec(query)
@@ -74,7 +75,7 @@ func dbxInsert(dbx *sqlx.DB, jsonbody string) {
 // // }
 // tx.Commit()
 
-func main() {
+func Action() {
 	dsn := "root:root@tcp(localhost:3306)/jarujaru"
 
 	dbx, dberr := NewMysqlConnect(dsn)
@@ -86,45 +87,10 @@ func main() {
 	dbx.SetConnMaxLifetime(time.Minute * 3)
 	dbx.SetMaxOpenConns(10)
 	dbx.SetMaxIdleConns(10)
-	//update
-
-	// tx := dbx.MustBegin()
-	// var name = "funase"
-	// //	for id := 5;id <10;id ++{
-	// //		tx.MustExec("insert into tests values(?,?)",id,name+strconv.Itoa(id))
-	// //	}
-	// for id := 5; id < 10; id++ {
-	// 	tx.MustExec("insert into tests(name) values(?)", name+strconv.Itoa(id))
-	// }
-	// tx.Commit()
-
-	// select
-	// type tests struct {
-	// 	Id   int
-	// 	Name string
-	// }
-
-	// result := []tests{}
-	// dbx.Select(&result, "select * from tests")
-	// for _, row := range result {
-
-	// 	fmt.Println(row)
-	// }
-	// type Results struct {
-	// 	Id   int
-	// 	Name string
-	// }
-
-	// rows := []Results{}
-	// dbx.Select(&rows, "select id,name from tests where id <= 10")
-	// for i, _ := range rows {
-	// 	fmt.Printf("取得したIDは「%d」：NAMEは「%s」です。\n", rows[i].Id, rows[i].Name)
-
-	//jsonbody := `{ "linkCnt":0,"comment":"good","time": "2022-06-25T00:22:50Z"}`
-	jsonbody := `[{ "linkCnt":0,"comment":"good","time": "2022-06-25T00:22:50Z"},{ "linkCnt":2,"comment":"nice","time": "2022-06-25T00:22:50Z"}]`
-	dbxInsert(dbx, jsonbody)
-	query := "select * from jarujaru"
-	dbxSelect(dbx, query)
+	// jsonbody := `[{ "linkCnt":0,"comment":"good","time": "2022-06-25T00:22:50Z"},{ "linkCnt":2,"comment":"nice","time": "2022-06-25T00:22:50Z"}]`
+	// DbxInsert(dbx, jsonbody)
+	// query := "select * from jarujaru"
+	// DbxSelect(dbx, query)
 
 	fmt.Println("succsess")
 
--- main.go
+++ main.go
@@ -1,84 +1,110 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"main/db"
+	"strings"
+	"time"
+
 	//"flag"
 
 	"log"
-	"net/http"
 
-	"google.golang.org/api/googleapi/transport"
+	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 )
 
-// var (
-// 	query      = flag.String("query", "Google", "Search term")
-// 	maxResults = flag.Int64("max-results", 25, "Max YouTube results")
-// )
+//やること
+//1動画のコメントを全て取得できるようにする。
+//-ちょうどいい動画を探す
+
+//動画 (ダイアン)
+//HK8CzJm8gdM
+//
 
 const developerKey = ""
 
 func createService() (service *youtube.Service) {
-	client := &http.Client{
-		Transport: &transport.APIKey{Key: developerKey},
-	}
-	service, err := youtube.New(client)
+	ctx := context.Background()
+	//service, err := youtube.NewService(ctx)
+	service, err := youtube.NewService(ctx, option.WithAPIKey(developerKey))
 	if err != nil {
 		log.Fatalf("Error creating new YouTube client: %v", err)
 	}
 	return service
 }
 
-func getCommentThread(nextPageToken, videoID string, service *youtube.Service) {
+func getCommentThread(nextPageToken, videoID string, service *youtube.Service, responseBodyArray []db.Comment) (responseBody []db.Comment, nextpagetoken string) {
 	// Make the API call to YouTube.
-	call := service.CommentThreads.List([]string{"snippet"}).
-		VideoId(videoID)
+	var call *youtube.CommentThreadsListCall
+	if nextPageToken == "" {
+		call = service.CommentThreads.List([]string{"snippet"}).
+			VideoId(videoID).MaxResults(50)
+	} else {
+		call = service.CommentThreads.List([]string{"snippet"}).
+			VideoId(videoID).PageToken(nextPageToken).MaxResults(50)
+	}
 	response, err := call.Do()
 	handleError(err, "")
-	fmt.Println(response.Items[0])
+	// body, _ := response.MarshalJSON()
 
+	nextpagetoken = response.NextPageToken
+	// fmt.Println("---------------")
+	// fmt.Println(string(body))
+	// fmt.Println("---------------")
+	// index := 0
+	//jst, _ := time.LoadLocation("Asia/Tokyo")
 	for _, item := range response.Items {
-		text := item.Snippet.TopLevelComment.Snippet.TextDisplay
-		likeCnt := item.Snippet.TopLevelComment.Snippet.LikeCount
-		time := item.Snippet.TopLevelComment.Snippet.PublishedAt
-		fmt.Printf("Text: %v\n", text)
-		fmt.Printf("likeCnt: %v\n", likeCnt)
-		fmt.Printf("time: %v\n", time)
+		commentInstance := db.Comment{}
+		commentInstance.Comment = strings.Replace(item.Snippet.TopLevelComment.Snippet.TextDisplay, "\"", "", -1)
+		commentInstance.LinkCnt = int(item.Snippet.TopLevelComment.Snippet.LikeCount)
+		commentInstance.Updated_time, _ = time.Parse(time.RFC3339, item.Snippet.TopLevelComment.Snippet.PublishedAt)
+		//commentInstance.Updated_time, _ = time.ParseInLocation("2006-01-02 15:04:05", item.Snippet.TopLevelComment.Snippet.PublishedAt, jst)
+		commentInstance.VideoID = item.Snippet.TopLevelComment.Snippet.VideoId
+		responseBodyArray = append(responseBodyArray, commentInstance)
+		// fmt.Printf("comment: %v\n", commentInstance.Comment)
+		// fmt.Printf("likeCnt: %v\n", commentInstance.LinkCnt)
+		// fmt.Printf("time: %v\n", commentInstance.Updated_time)
+		// fmt.Printf("videoID: %v\n", commentInstance.VideoID)
+
 	}
+	// fmt.Println(index)
+	return responseBodyArray, nextpagetoken
 }
 
-func getVideoIDs(uploadsID string, service *youtube.Service) (videoIDs []string) {
-	playlistsCall := service.PlaylistItems.List([]string{"contentDetails"}).PlaylistId(uploadsID).MaxResults(50)
+// func getVideoIDs(uploadsID string, service *youtube.Service) (videoIDs []string) {
+// 	playlistsCall := service.PlaylistItems.List([]string{"contentDetails"}).PlaylistId(uploadsID).MaxResults(50)
 
-	playlistsResponse, err := playlistsCall.Do()
-	if err != nil {
-		log.Fatalf("Error call YouTube API: %v", err)
-	}
+// 	playlistsResponse, err := playlistsCall.Do()
+// 	if err != nil {
+// 		log.Fatalf("Error call YouTube API: %v", err)
+// 	}
 
-	for _, item := range playlistsResponse.Items {
-		videoIDs = append(videoIDs, item.ContentDetails.VideoId)
-	}
+// 	for _, item := range playlistsResponse.Items {
+// 		videoIDs = append(videoIDs, item.ContentDetails.VideoId)
+// 	}
 
-	if playlistsResponse.NextPageToken != "" {
-		nextPageToken := playlistsResponse.NextPageToken
-		for {
-			nextCall := service.PlaylistItems.List([]string{"contentDetails"}).PlaylistId(uploadsID).PageToken(nextPageToken).MaxResults(50)
-			nextResponse, err := nextCall.Do()
-			if err != nil {
-				log.Fatalf("Error call YouTube API: %v", err)
-			}
-			for _, nextItem := range nextResponse.Items {
-				videoIDs = append(videoIDs, nextItem.ContentDetails.VideoId)
-			}
-			nextPageToken = nextResponse.NextPageToken
-			if nextPageToken == "" {
-				break
-			}
-		}
-	}
+// 	if playlistsResponse.NextPageToken != "" {
+// 		nextPageToken := playlistsResponse.NextPageToken
+// 		for {
+// 			nextCall := service.PlaylistItems.List([]string{"contentDetails"}).PlaylistId(uploadsID).PageToken(nextPageToken).MaxResults(50)
+// 			nextResponse, err := nextCall.Do()
+// 			if err != nil {
+// 				log.Fatalf("Error call YouTube API: %v", err)
+// 			}
+// 			for _, nextItem := range nextResponse.Items {
+// 				videoIDs = append(videoIDs, nextItem.ContentDetails.VideoId)
+// 			}
+// 			nextPageToken = nextResponse.NextPageToken
+// 			if nextPageToken == "" {
+// 				break
+// 			}
+// 		}
+// 	}
 
-	return videoIDs
-}
+// 	return videoIDs
+// }
 
 type VideoData struct {
 	VideoID       string
@@ -91,80 +117,132 @@ type VideoData struct {
 	PublishedAt   string
 }
 
-func getVideoDatas(videoIDs []string, service *youtube.Service) (videoDatas []VideoData) {
-	for _, videoID := range videoIDs {
-		call := service.Videos.List([]string{"snippet", "statistics"}).Id(videoID)
-		response, err := call.Do()
-		if err != nil {
-			log.Fatalf("Error call YouTube API: %v", err)
-		}
+// func getVideoDatas(videoIDs []string, service *youtube.Service) (videoDatas []VideoData) {
+// 	for _, videoID := range videoIDs {
+// 		call := service.Videos.List([]string{"snippet", "statistics"}).Id(videoID)
+// 		response, err := call.Do()
+// 		if err != nil {
+// 			log.Fatalf("Error call YouTube API: %v", err)
+// 		}
 
-		item := response.Items[0]
-		videoData := VideoData{
-			VideoID:       videoID,
-			Title:         item.Snippet.Title,
-			ViewCount:     item.Statistics.ViewCount,
-			LikeCount:     item.Statistics.LikeCount,
-			DislikeCount:  item.Statistics.DislikeCount, // ※oauth認証じゃないと取得できない
-			FavoriteCount: item.Statistics.FavoriteCount,
-			CommentCount:  item.Statistics.CommentCount,
-			PublishedAt:   item.Snippet.PublishedAt,
-		}
-		videoDatas = append(videoDatas, videoData)
-	}
-	return videoDatas
-}
+// 		item := response.Items[0]
+// 		videoData := VideoData{
+// 			VideoID:       videoID,
+// 			Title:         item.Snippet.Title,
+// 			ViewCount:     item.Statistics.ViewCount,
+// 			LikeCount:     item.Statistics.LikeCount,
+// 			DislikeCount:  item.Statistics.DislikeCount, // ※oauth認証じゃないと取得できない
+// 			FavoriteCount: item.Statistics.FavoriteCount,
+// 			CommentCount:  item.Statistics.CommentCount,
+// 			PublishedAt:   item.Snippet.PublishedAt,
+// 		}
+// 		videoDatas = append(videoDatas, videoData)
+// 	}
+// 	return videoDatas
+// }
 
 func main() {
 	// flag.Parse()
-	videoid := "9BBoWAANXHo"
+	videoid := "HK8CzJm8gdM"
 	service := createService()
-	call := service.CommentThreads.List([]string{"snippet"}).
-		VideoId(videoid).MaxResults(50)
-	response, err := call.Do()
-	handleError(err, "")
-	fmt.Println(response.Items[0])
-	if response.NextPageToken != "" {
-		nextPageToken := response.NextPageToken
-		fmt.Printf("nextPageToken: %v\n", nextPageToken)
-		index := 1
-		for {
-			call := service.CommentThreads.List([]string{"snippet"}).
-				VideoId(videoid).
-				PageToken(nextPageToken)
-			response, err := call.Do()
-			handleError(err, "")
-			// fmt.Println(response.Items[0])
+	nextPageToken := ""
+	var responseBodyArray []db.Comment
+	index := 0
+	for {
+		index += 1
 
-			// for _, item := range response.Items {
-			// 	text := item.Snippet.TopLevelComment.Snippet.TextDisplay
-			// 	likeCnt := item.Snippet.TopLevelComment.Snippet.LikeCount
-			// 	time := item.Snippet.TopLevelComment.Snippet.PublishedAt
-			// 	fmt.Printf("Text: %v\n", text)
-			// 	fmt.Printf("likeCnt: %v\n", likeCnt)
-			// 	fmt.Printf("time: %v\n", time)
-			// }
-			nextPageToken := response.NextPageToken
-			if nextPageToken == "" {
-				break
-			}
-			fmt.Printf("nextPageToken: %v\n", nextPageToken)
-			index += 1
-			if index == 3 {
-				break
-			}
+		//responseBodyArray, nextPageToken := getCommentThread(nextPageToken, videoid, service)
+		responseBodyArray, nextPageToken = getCommentThread(nextPageToken, videoid, service, responseBodyArray)
+		fmt.Println(len(responseBodyArray))
+		// fmt.Println("-----------------------------------------")
+		fmt.Println(index)
+		// fmt.Println(nextPageToken)
 
+		// fmt.Println("-----------------------------------------")
+		if index == 5 {
+			break
 		}
-
+		if nextPageToken == "" {
+			break
+		}
 	}
+	fmt.Println(len(responseBodyArray))
+	fmt.Println(responseBodyArray)
+	//dbに保存
+	dsn := "root:root@tcp(localhost:3306)/jarujaru"
+
+	dbx, dberr := db.NewMysqlConnect(dsn)
+	if dberr != nil {
+		fmt.Println("err")
+		panic(dberr)
+	}
+	defer dbx.Close()
+	dbx.SetConnMaxLifetime(time.Minute * 3)
+	dbx.SetMaxOpenConns(10)
+	dbx.SetMaxIdleConns(10)
+
+	db.DbxInsert(dbx, responseBodyArray)
+
+	query := "select * from jarujaru"
+	db.DbxSelect(dbx, query)
+
+	// call := service.CommentThreads.List([]string{"snippet"}).
+	// 	VideoId(videoid).MaxResults(50)
+	// response, err := call.Do()
+	// handleError(err, "")
+	// fmt.Println(response.Items[0])
+	// if response.NextPageToken != "" {
+	// 	nextPageToken := response.NextPageToken
+	// 	fmt.Printf("nextPageToken: %v\n", nextPageToken)
+	// index := 1
+	// for {
+	// 	call := service.CommentThreads.List([]string{"snippet"}).
+	// 		VideoId(videoid).
+	// 		PageToken(nextPageToken)
+	// 	response, err := call.Do()
+	// 	handleError(err, "")
+	// 	// fmt.Println(response.Items[0])
+
+	// 	// for _, item := range response.Items {
+	// 	// 	text := item.Snippet.TopLevelComment.Snippet.TextDisplay
+	// 	// 	likeCnt := item.Snippet.TopLevelComment.Snippet.LikeCount
+	// 	// 	time := item.Snippet.TopLevelComment.Snippet.PublishedAt
+	// 	// 	fmt.Printf("Text: %v\n", text)
+	// 	// 	fmt.Printf("likeCnt: %v\n", likeCnt)
+	// 	// 	fmt.Printf("time: %v\n", time)
+	// 	// }
+	// 	nextPageToken := response.NextPageToken
+	// 	if nextPageToken == "" {
+	// 		break
+	// 	}
+	// 	fmt.Printf("nextPageToken: %v\n", nextPageToken)
+	// 	index += 1
+	// 	if index == 3 {
+	// 		break
+	// 	}
+
+	//	}
+
+	// }
 
 }
 
-//linkカウントも取得する
-//綺麗にする
+//linkカウントも取得する ok
+//綺麗にする　ok
 
+//動画のURLを書く
+//取得したコメントをデータベースに保存する
 //DBに保存する
+
+//時間を曜日で分ける
+//月で分ける
+//時間で分ける
+
 //より簡単にする
+
+//typeを分類する方法を考える
+//デザインを考える。
+//動画を毎日保存する。
 
 // 	// Group video, channel, and playlist results in separate lists.
 // 	videos := make(map[string]string)
